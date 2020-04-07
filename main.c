07cc14d1dd859f52357caa86a3cf35483810fa0e
ykaridi <ykaridi@gmail.com>
2020-04-07 19:26:46 +0300

    quick fix

--- main.c
+++ main.c
@@ -54,7 +54,7 @@ int main() {
         command_status = (*command).function(game, &command_arguments);
         if (command_status == BOARD_UPDATE || command_status == PARAMETER_UPDATE) {
             print_board(game->board, game->mode == EDIT || game->mark_errors);
-            if (is_board_full(game->board)) {
+            if (game->mode == SOLVE && is_board_full(game->board)) {
                 if (check_board(game->board)) {
                     printf("Board was solved successfully!\n");
                     game->mode = INIT;
